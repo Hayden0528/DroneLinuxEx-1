2806b14e72b7829413dc1fd4b9559de1a3a73feb
venture21 <park0122@gmail.com>
2018-07-11 15:13:04 +0900

    add threadAdd.c

--- thread/threadAdd.c
+++ thread/threadAdd.c
@@ -2,6 +2,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/time.h>
+#include <time.h>
+
+//#define DEBUG
+
 
 struct Num
 {
@@ -18,9 +23,10 @@ void* add_func(void* data)
 
 	num1 = *((struct Num*)data);
 
+#ifdef DEBUG
 	printf("num1.start=%d\n", num1.start);
 	printf("num1.end=%d\n", num1.end);
-
+#endif
 	for(i=num1.start;i<=num1.end;i++)
 	{
 		result += i;
@@ -29,7 +35,18 @@ void* add_func(void* data)
 	return (void*)(result);
 }
 
+long adder(int start, int end)
+{
+        int i;
+        long long result=0;
 
+        for(i = start ;i<=end; i++)
+        {
+                result += i;
+        }
+
+        return result;
+}
 
 
 int main(int argc, char *argv[])
@@ -38,32 +55,65 @@ int main(int argc, char *argv[])
 	struct Num num1, num2;
 	long sum1, sum2;
 	long result;
-	
-	num1.start = 1;
-	num2.end = (atoi(argv[1]));
-	num1.end = (num2.end>>1);
-	num2.start = num1.end+1;
+	int threadNum;
+	struct timeval StartTime, EndTime, ResultTime;	
 
-	if(pthread_create(&p_thread[0], NULL, add_func, (void*)&num1)<0)
+	threadNum = atoi(argv[1]);
+
+	
+	gettimeofday(&StartTime, NULL);
+
+	switch(threadNum)
 	{
-		perror("thread1 create error:");
-		exit(1);
+		case 1: result = adder(1, atoi(argv[2]));
+			break;
+		case 2:
+		        num1.start = 1;
+        		num2.end = (atoi(argv[2]));
+        		num1.end = (num2.end>>1);
+        		num2.start = num1.end+1;
+			if(pthread_create(&p_thread[0], NULL, add_func, (void*)&num1)<0)
+			{
+				perror("thread1 create error:");
+				exit(1);
+			}
+
+        		if(pthread_create(&p_thread[1], NULL, add_func, (void*)&num2)<0)
+        		{
+                		perror("thread2 create error:");
+                		exit(2);
+        		}
+
+			pthread_join(p_thread[0],(void**)&sum1);
+			pthread_join(p_thread[1],(void**)&sum2);
+
+			result = sum1+sum2;
+#ifdef DEBUG
+			printf("sum1: %ld\n", sum1);
+			printf("sum2: %ld\n", sum2);
+#endif
+			break;
+		default:
+			break;
 	}
 
-        if(pthread_create(&p_thread[1], NULL, add_func, (void*)&num2)<0)
-        {
-                perror("thread2 create error:");
-                exit(2);
-        }
+	gettimeofday(&EndTime, NULL);
 
-	pthread_join(p_thread[0],(void**)&sum1);
-	pthread_join(p_thread[1],(void**)&sum2);
-
-	result = sum1+sum2;
-	printf("sum1: %ld\n", sum1);
-	printf("sum2: %ld\n", sum2);
 	printf("Result : %ld\n", result);
 
+	// 마이크로초 계산
+	if(EndTime.tv_usec<StartTime.tv_usec)
+	{
+		EndTime.tv_sec--;
+		ResultTime.tv_usec = (1000000 + EndTime.tv_usec) - StartTime.tv_usec;
+	}
+	else
+		ResultTime.tv_usec = EndTime.tv_usec - StartTime.tv_usec;
+
+	// 초 계산
+	ResultTime.tv_sec = EndTime.tv_sec - StartTime.tv_sec;
+	
+	printf("Running Time : %ld.%ld\n", ResultTime.tv_sec,ResultTime.tv_usec);
 	return 0;
 }
 
