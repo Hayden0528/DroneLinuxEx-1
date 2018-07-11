b97ccd16f5d5163ec5c31a744ec18d96d7679784
venture21 <park0122@gmail.com>
2018-07-11 14:41:22 +0900

    update timeEx.c

--- timeEx.c
+++ timeEx.c
@@ -15,16 +15,19 @@ int main(int argc, char *argv[])
 	time(&UTCtime);
 	printf("time : %u\n", (unsigned)UTCtime);	// UTC 현재시간 출력
 	
+	// 2. localtime()
 	tm = localtime(&UTCtime);
 	printf("year:%d\n", tm->tm_year);
 
+	// 3. asctime()
 	printf("asctime: %s",asctime(tm)); //현재 시간을 tm구조체에서 읽어와서 출력
 
+	// 4. strftime()
 	strftime(buf, sizeof(buf), "%Y-%m-%e %H:%M:%S %A", tm);
 	printf("strftime : %s\n", buf);
 
 
-	// 2. gettimeofday()
+	// 5. gettimeofday()
 	gettimeofday(&UTCtime_u, NULL);	    // UTC 현재시간(마이크로초까지)
 	printf("gettimeofday : %ld/%ld\n", UTCtime_u.tv_sec, UTCtime_u.tv_usec);
 	return 0;
